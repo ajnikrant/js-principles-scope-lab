ffcfa2ae880603bec599f5b4e3c029d28b9c13ba
Amanda <ajnikrant@gmail.com>
2021-04-04 17:43:01 -0400

    commit tree f7d08421234a0456a99a71baa5641e2fbb4c93c5

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+var customerName= "bob"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase
+}
+function setBestCustomer(){
+    var bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'bob again'
+}
